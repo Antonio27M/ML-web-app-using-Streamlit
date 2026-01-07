1ccc67466de6bcdbb576da812f8290515bfd0654
Antonio Martinez <153334216+Antonio27M@users.noreply.github.com>
2026-01-07 18:02:27 +0000

    web app

--- src/app.py
+++ src/app.py
@@ -26,12 +26,12 @@ val6 = st.slider('citric acid', min_value=0.00000, max_value=1.00000, step=0.1)
 val7 = st.slider('chlorides', min_value=0.01200, max_value=0.61100, step=0.1)
 val8 = st.slider('free sulfur dioxide', min_value=1.00000, max_value=72.00000, step=0.1)
 val9 = st.slider('total sulfur dioxide', min_value=6.00000, max_value=289.00000, step=0.1)
-val10 = st.slider('density', min_value=0.99007, max_value=1.00369, step=0.1)
+val10 = st.slider('density', min_value=0.99007, max_value=1.00369, step=0.001)
 val11 = st.slider('sulphates', min_value=0.33000, max_value=2.00000, step=0.1)
 
 
 if st.button("Predict"):
-    prediction = str(model.predict([[val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val12]])[0])
+    prediction = str(model.predict([[val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11]])[0])
     pred_class = class_dict[prediction]
     st.divider()
     st.write("Prediction:", pred_class)
