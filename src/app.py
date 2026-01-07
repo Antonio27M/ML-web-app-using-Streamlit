db13e356cec0bfcb4c39881f226a7472f3a9b476
Antonio Martinez <153334216+Antonio27M@users.noreply.github.com>
2026-01-07 18:06:04 +0000

    web app

--- src/app.py
+++ src/app.py
@@ -19,15 +19,15 @@ st.divider()
 
 val1 = st.slider('alcohol', min_value=8.40000, max_value=14.90000, step=0.1)
 val2 = st.slider('pH', min_value=2.74000, max_value=4.01000, step=0.1)
-val3 = st.slider('residual sugar', min_value=0.90000, max_value=15.50000, step=0.1)
+val3 = st.slider('residual sugar', min_value=0.90000, max_value=15.50000, step=0.00001)
 val4 = st.slider('fixed acidity', min_value=4.60000, max_value=15.90000, step=0.1)
 val5 = st.slider('volatile acidity', min_value=0.12000, max_value=1.58000, step=0.1)
-val6 = st.slider('citric acid', min_value=0.00000, max_value=1.00000, step=0.1)
-val7 = st.slider('chlorides', min_value=0.01200, max_value=0.61100, step=0.1)
+val6 = st.slider('citric acid', min_value=0.00000, max_value=1.00000, step=0.0001)
+val7 = st.slider('chlorides', min_value=0.01200, max_value=0.61100, step=0.0001)
 val8 = st.slider('free sulfur dioxide', min_value=1.00000, max_value=72.00000, step=0.1)
 val9 = st.slider('total sulfur dioxide', min_value=6.00000, max_value=289.00000, step=0.1)
-val10 = st.slider('density', min_value=0.99007, max_value=1.00369, step=0.001)
-val11 = st.slider('sulphates', min_value=0.33000, max_value=2.00000, step=0.1)
+val10 = st.slider('density', min_value=0.99007, max_value=1.00369, step=0.00001)
+val11 = st.slider('sulphates', min_value=0.33000, max_value=2.00000, step=0.00001)
 
 
 if st.button("Predict"):
